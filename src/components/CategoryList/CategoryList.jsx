a0fe3c813e8f98f076fed93bc1e1bd6edb7665c6
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-09-22 19:18:37 +0600

    Load Jobs data for the Page

---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,10 @@
+const CategoryList = () => {
+    return (
+        <div>
+            <h2 className="text-5xl text-center">Job Category List</h2>
+            <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime praesentium officia quia maiores, natus temporibus ratione pariatur voluptas fugit cumque consequatur amet blanditiis. Architecto, reiciendis! Quia eligendi adipisci modi vel.</p>            
+        </div>
+    );
+};
+
+export default CategoryList;
---
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import Jobs from "../Jobs/Jobs";
+
+const FeaturedJobs = () => {
+
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(()=>{
+        fetch('jobs.json')
+        .then(res => res.json())
+        .then(data => setJobs(data))
+    },[])
+
+    return (
+        <div>
+            <div className="text-center">
+                <div className="text-5xl">Featured Jobs: {jobs.length}</div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, neque! Incidunt cupiditate earum quia magnam ab, </p>
+            </div>
+            <div>
+                {
+                    jobs.map(job => <Jobs job={job} key={job.id}></Jobs>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedJobs;

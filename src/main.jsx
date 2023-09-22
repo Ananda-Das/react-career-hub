871c2a0fc49fd572d928bf547907261a64089267
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-09-22 19:03:48 +0600

    Set Nested Route and Navbar for CareerHub

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter,
@@ -8,15 +7,21 @@ import {
 } from "react-router-dom";
 import Root from './components/Root/Root.jsx';
 import Home from './components/Home/Home.jsx';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs.jsx';
+import ErrorPage from './components/ErrorPage/ErrorPage.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children:[
       {
         path: '/',
         element: <Home></Home>
+      },{
+        path: '/applied',
+        element: <AppliedJobs></AppliedJobs>
       }
     ]
   },

4f01465b50b621b09086c4323535edef37477150
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-09-22 21:46:47 +0600

    Add icons and handle show all jobs

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -2,28 +2,34 @@ import { useEffect, useState } from "react";
 import Jobs from "../Jobs/Jobs";
 
 const FeaturedJobs = () => {
+  const [jobs, setJobs] = useState([]);
 
-    const [jobs, setJobs] = useState([]);
+  const [dataLength, setDataLength] = useState(4);
 
-    useEffect(()=>{
-        fetch('jobs.json')
-        .then(res => res.json())
-        .then(data => setJobs(data))
-    },[])
+  useEffect(() => {
+    fetch("jobs.json")
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
 
-    return (
-        <div>
-            <div className="text-center">
-                <div className="text-5xl">Featured Jobs: {jobs.length}</div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, neque! Incidunt cupiditate earum quia magnam ab, </p>
-            </div>
-            <div className="grid grid-cols-2 gap-6">
-                {
-                    jobs.map(job => <Jobs job={job} key={job.id}></Jobs>)
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="text-center">
+        <div className="text-5xl">Featured Jobs: {jobs.length}</div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, neque! Incidunt cupiditate earum quia magnam ab, </p>
+      </div>
+      <div className="grid grid-cols-2 gap-6">
+        {jobs.slice(0, dataLength).map((job) => (
+          <Jobs job={job} key={job.id}></Jobs>
+        ))}
+      </div>
+      <div className={dataLength === jobs.length ? "hidden" : ""}>
+        <button onClick={() => setDataLength(jobs.length)} className="btn btn-primary">
+          See All Jobs
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default FeaturedJobs;
--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -1,3 +1,6 @@
+import { MdLocationOn } from "react-icons/md";
+import { BsCurrencyDollar } from "react-icons/bs";
+
 const Jobs = ({ job }) => {
   const { logo, job_title, remote_or_onsite, company_name, location, job_type, salary } = job;
   return (
@@ -11,6 +14,10 @@ const Jobs = ({ job }) => {
         <div>
             <button className="px-5 py-2 font-extrabold border rounded border-[#7e90fe] mr-4 text-[#7e90fe]">{remote_or_onsite}</button>
             <button className="px-5 py-2 font-extrabold border rounded border-[#7e90fe] mr-4 text-[#7e90fe]">{job_type}</button>
+        </div>
+        <div className="mt-4 flex">
+            <h2 className="flex"><MdLocationOn className="text-2xl mr-2"></MdLocationOn> {location}</h2>
+            <h2 className="flex "><BsCurrencyDollar className="text-2xl mr-2"></BsCurrencyDollar> {salary}</h2>
         </div>
         <div className="card-actions">
           <button className="btn btn-primary">View Details</button>
